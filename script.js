f9d7f2917a812ef0028d4a5ea4c72999fffb310e
ResourceMined <129284414+ResourceMined@users.noreply.github.com>
2024-05-17 15:30:52 +0800

    inital COmmit

---
+++ script.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const https = require('https');
+require('dotenv').config();  // Load environment variables from .env file
+
+const app = express();
+
+const API_URL = 'https://opstanamitest.newmont.com/api/external/v3.7/ShiftWorkItems';
+const API_TOKEN = process.env.API_TOKEN;  // Access the API token from environment variables
+
+// Use CORS middleware
+app.use(cors());
+
+// Create an https agent to ignore SSL certificate
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
+
+app.get('/proxy/tasks', async (req, res) => {
+  try {
+    const { StartDate, EndDate } = req.query;
+    const response = await axios.get(API_URL, {
+      headers: {
+        'Accept': 'application/json',
+        'ApiToken': API_TOKEN
+      },
+      params: {
+        StartDate,
+        EndDate
+      },
+      httpsAgent: agent
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      console.error('Response data:', error.response.data);
+      console.error('Response status:', error.response.status);
+      console.error('Response headers:', error.response.headers);
+      res.status(error.response.status).send(error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('Request data:', error.request);
+      res.status(500).send('No response received from API');
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error message:', error.message);
+      res.status(500).send('Error in setting up API request');
+    }
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
